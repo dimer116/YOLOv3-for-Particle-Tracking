e4fa76ba63638e275311ef6a0d3f1f45481dcfab
dimer116 <edvinjohansson116@gmail.com>
2021-05-21 10:35:07 +0200

    Update SimulateTool.py
    Removed arcaic functions and cleaned up. Removed swedish explainations

--- simulation/SimulateTool.py
+++ simulation/SimulateTool.py
@@ -1,8 +1,9 @@
 import numpy as np
 import deeptrack as dt
 import StructuralNoise as SN
-from scipy.optimize import curve_fit
 
+# Simulate Tool for generating images with the help of Deeptrack.
+# Change parameters for the simulated images here.
 
 class SimulateTool():
     def __init__(
@@ -29,11 +30,7 @@ class SimulateTool():
         self.resolution = 1.14e-6
         self.magnification = 10
 
-        pupil = dt.HorizontalComa(
-            coefficient=lambda: 0.2 + 0.6 * np.random.rand()) \
-            + dt.VerticalComa(
-                    coefficient=lambda: 0.2 + 0.6 * np.random.rand())
-
+        # Particle parameters
         particle = dt.MieSphere(
             refractive_index=lambda: np.random.uniform(1.37, 1.7),
             refractive_index_medium=1.33,
@@ -51,6 +48,13 @@ class SimulateTool():
             #L=5
         )
 
+        # Adds coma aberration
+        pupil = dt.HorizontalComa(
+            coefficient=lambda: 0.2 + 0.6 * np.random.rand()) \
+                + dt.VerticalComa(
+            coefficient=lambda: 0.2 + 0.6 * np.random.rand())
+
+        # Illumination gradient
         if addIllumination:
             illumination_gradient = dt.IlluminationGradient(
                 gradient=lambda: (np.random.randn(2)) * 5e-7)
@@ -58,6 +62,7 @@ class SimulateTool():
             illumination_gradient = dt.IlluminationGradient(
                 gradient=[0, 0])
 
+        # Noise added together with illumination gradient
         sigma_max = 0
         if addNoise:
             sigma_max = 5e-2
@@ -68,6 +73,7 @@ class SimulateTool():
             mu=0,
             sigma=lambda: np.random.rand(1) * 1j * sigma_max)
 
+        # Microscope parameters
         brightfield_microscope = dt.Brightfield(
             NA=1.3,
             aperature_angle=53.7 * 2 * np.pi / 360,
@@ -113,16 +119,9 @@ class SimulateTool():
         ''' Returns the structura noise class variable '''
         return self.struc_noise
 
-    # Retunerar arrayen med bilderna.
     def getImages(self):
         ''' Returns the array of generated images '''
         return self.images
-
-    # Could probably be removed, serves really no purpose atm.
-    def getImageAmplitude(self, image):
-        ''' Returns the amplitude of the given image. Same as
-            np.abs(image). '''
-        return np.abs(image)
 
     def getMaxImageAmplitude(self, image):
         ''' Returns the maximum amplitud of an image '''
@@ -132,10 +131,6 @@ class SimulateTool():
             if MaxImageK > max:
                 max = MaxImageK
         return max
-
-    def getRefractiveIndex(self, image):
-        ''' ??? '''
-        return image.properties[0]['refractive_index']
 
     def getIllumination(self, image):
         ''' Returns the illumination gradient of the given image '''
@@ -151,8 +146,6 @@ class SimulateTool():
             self.positions = old_positions
         return image
 
-    # Retunerar feature:n som ansvarar för bild-genereringen. Lämpligt vid
-    # användning av Deeptracks generators.
     def getSimulationFeature(self):
         ''' Returns the deeptrack feature responsible for generating images '''
         return self.feature
@@ -174,43 +167,6 @@ class SimulateTool():
         noisy_image.properties = image.properties
 
         return noisy_image
-
-    def find_peak_width(self, output_image, number_of_peaks=4,
-                        check_length=40) -> list:
-        widths = []
-        image = np.squeeze(output_image)
-        image_size = self.IMAGE_X_SIZE
-        positions = self.get_pos(output_image)
-
-        def sine_fit(x, amp_offset, amp, k, phase):
-            return amp_offset + amp * np.sin(2 * np.pi * k * x + phase)
-
-        for pos in positions:
-            x_pos = int(pos[0])
-            y_pos = int(pos[1])
-
-            if x_pos > image_size / 2:
-                image_x = image[(x_pos - check_length):x_pos, y_pos]
-            else:
-                image_x = image[x_pos:(x_pos + check_length), y_pos]
-            if y_pos > image_size / 2:
-                image_y = image[x_pos, (y_pos - check_length):y_pos]
-            else:
-                image_y = image[x_pos, y_pos:(y_pos + check_length)]
-
-            image_average = (image_y + image_x) / 2
-
-            init_vals = [1, 0.1, 0.25, 0]  # for [amp_offset, amp, k, phase]
-            bounds = ([0.8, 0.01, 0.1, -np.pi], [1.2, 0.2, 0.5, np.pi])
-            try:
-                best_vals, covar = curve_fit(sine_fit, np.arange(check_length),
-                                             image_average, p0=init_vals,
-                                             bounds=bounds)
-            except RuntimeError:
-                widths.append(32)
-                continue
-            widths.append(number_of_peaks * 2 / best_vals[2])
-        return widths
 
     def random_position(self, limit, image_size_x, image_size_y):
         ''' Generates a new position with a distance above specified limit
@@ -258,7 +214,6 @@ class SimulateTool():
         for properties in resolved_image.properties:
             if "position" in properties:
                 tempPos = properties["position"]
-                # v = np.append(v, properties["position"] / self.IMAGE_X_SIZE)
                 v = np.append(v, [tempPos[0] / self.IMAGE_X_SIZE,
                                   tempPos[1] / self.IMAGE_Y_SIZE])
                 v = np.append(v, properties["z"])
@@ -269,16 +224,3 @@ class SimulateTool():
         v = np.reshape(v, (int(s[0] / 5), 5))
         return v
 
-    def get_label(self, resolved_image):
-        label = []
-        positions = self.get_all_pos(resolved_image)
-        widths = self.find_peak_width(resolved_image)
-        for i in range(np.size(widths)):
-            label.append([0,
-                          positions[i][0] / self.IMAGE_X_SIZE,
-                          positions[i][1] / self.IMAGE_Y_SIZE,
-                          positions[i][2],
-                          positions[i][3],
-                          widths[i] / self.IMAGE_X_SIZE,
-                          widths[i] / self.IMAGE_Y_SIZE])
-        return label
